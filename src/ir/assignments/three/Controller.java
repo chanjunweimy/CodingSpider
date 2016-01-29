4276dc0f699b618505ce9ef33feed0e8fd21cd89
Chan Jun Wei <chanjunweimy@gmail.com>
2016-01-29 08:57:23 +0800

    manually config the crawler
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- src/ir/assignments/three/Controller.java
+++ src/ir/assignments/three/Controller.java
@@ -9,12 +9,37 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class Controller {
-	public boolean startController (String seedURL) {
-		 String crawlStorageFolder = "/data";
-	        int numberOfCrawlers = 7;
+	
+	/**
+	 * (Very important for politeness) Wait at least 600ms between page requests 
+	 * 	to the same subdomain. You need to set this in the crawler configuration.
+	 */
+	private final static int POLITENESS_DELAY = 600;
+	
+	/**
+	 * (Very important for getting credit) Set the name of your crawler's User Agent to 
+	 * this precise string: UCI Inf141-CS121 crawler StudentID(s), where the last part 
+	 * is the eight-digit student ID of each team member, separated by one space.
+	 */
+	private final static String USER_AGENT_STRING = "UCI Inf141-CS121 crawler 13793954";
+	
+	/**
+	 * The folder stores everything that we obtained from crawling
+	 */
+	public final static String CRAWL_STORAGE_FOLDER = "/data";
 
-	        CrawlConfig config = new CrawlConfig();
-	        config.setCrawlStorageFolder(crawlStorageFolder);
+	private final static int NUMBER_OF_CRAWLER = 7;
+	
+	
+	/**
+	 * This method is used to start the controller
+	 * @param seedURL url to start
+	 * @return
+	 */
+	public boolean startController (String seedURL) {
+
+	        CrawlConfig config = getTheConfigOfTheCrawler();
+
 
 	        /*
 	         * Instantiate the controller for this crawl.
@@ -42,8 +67,19 @@ public class Controller {
 	         * Start the crawl. This is a blocking operation, meaning that your code
 	         * will reach the line after this only when crawling is finished.
 	         */
-	        controller.start(CrawlerLibrary.class, numberOfCrawlers);
+	        controller.start(CrawlerLibrary.class, NUMBER_OF_CRAWLER);
 	        return CrawlerLibrary.getCrawledUrls() != null;
+	}
+
+	/**
+	 * @return
+	 */
+	public CrawlConfig getTheConfigOfTheCrawler() {
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(CRAWL_STORAGE_FOLDER);
+		config.setUserAgentString(USER_AGENT_STRING);
+		config.setPolitenessDelay(POLITENESS_DELAY);
+		return config;
 	}
 	
 	public ArrayList<String> getCrawledUrls() {
